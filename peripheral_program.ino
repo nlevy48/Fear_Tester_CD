832aae05edc5e36576cf27c3f4124a3c3eb8e78b
nlevy48 <155464271+nlevy48@users.noreply.github.com>
2024-09-18 09:35:05 -0400

    commit 9:34 09182024

--- peripheral_program.ino
+++ peripheral_program.ino
@@ -1,12 +1,12 @@
 // Arduino code
 
 // Define the sensor pin
-const int moisturePin = A0;
-const int heartRatePin = A1;
-const int stressguagePin = A2;
-const int redPin = 3;
-const int greenPin = 5;
-const int bluePin = 6;
+  const int moisturePin = A0;
+  const int heartRatePin = A1;
+  const int stressguagePin = A2;
+  const int whitePin = 3;
+  const int yellowPin = 5;
+  const int redPin = 6;
 
 void setup() {
   // Start the Serial communication to send data to the computer
@@ -24,6 +24,15 @@ void loop() {
 
   // Print the sensor value to the Serial Monitor
   Serial.println(heartRateValue);
+
+  // Input the sensor value to the LED
+  analogWrite(whitePin, 255);
+
+  // Input the sensor value to the LED
+  analogWrite(yellowPin, 255);
+
+  // Input the sensor value to the LED
+  analogWrite(redPin, 255);
 
   // Wait for a second before reading the sensor again
   delay(1000);
